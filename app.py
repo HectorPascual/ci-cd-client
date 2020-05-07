9a189b2de3e7167067822c25990ea3b70cc67b42
pascual <hector.pascual@silicon-gears.com>
2020-05-07 12:29:19 +0200

    Implemented node getting and job creating using HTML form for performing POST

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 import requests
 from flask import render_template
 app = Flask(__name__)
@@ -12,10 +13,17 @@ def hello():
 def test():
     return "Whats up!"
 
-@app.route('/jobs')
+@app.route('/jobs', methods=['GET','POST'])
 def jobs():
-    jobs = requests.get('http://localhost:5000/jobs').json()
-    return render_template("index.html", jobs=jobs)
+    if request.method == 'GET':
+        jobs = requests.get('http://localhost:5000/jobs').json()
+        return render_template("index.html", jobs=jobs)
+    elif request.method == 'POST':
+        data = {'title':request.form.get('title'),
+                'description':request.form.get('description')}
+        requests.post('http://localhost:5000/jobs',data=data)
+        jobs = requests.get('http://localhost:5000/jobs').json()
+        return render_template("index.html", jobs=jobs)
 
 @app.route('/jobs/<int:job_id>')
 def job(job_id):
@@ -24,6 +32,14 @@ def job(job_id):
 
     return render_template("index.html", job=job, builds=builds)
 
+@app.route('/nodes')
+def nodes():
+    nodes = requests.get('http://localhost:5000/nodes').json()
+    return render_template("index.html", nodes=nodes)
+
+@app.route('/create_job')
+def create_job_view():
+    return render_template("index.html", create_job=True)
 
 if __name__ == '__main__':
     app.run(port=8000, debug=False)
